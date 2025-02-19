ae34c958384506cdbbc137e7ca00c3b0dee352be
Viktor Pettersson <viktor.pettersson@portainer.io>
2025-02-19 02:46:39 +0100

    fix(edge): configure persisted mTLS certificates on start-up [BE-11622] (#437)
    Co-authored-by: andres-portainer <andres-portainer@users.noreply.github.com>
    Co-authored-by: oscarzhou <oscar.zhou@portainer.io>
    Co-authored-by: Oscar Zhou <100548325+oscarzhou-portainer@users.noreply.github.com>

--- api/portainer.go
+++ api/portainer.go
@@ -1493,7 +1493,8 @@ type (
 		StoreSSLCertPair(cert, key []byte) (string, string, error)
 		CopySSLCertPair(certPath, keyPath string) (string, string, error)
 		CopySSLCACert(caCertPath string) (string, error)
-		StoreMTLSCertificates(cert, caCert, key []byte) (string, string, string, error)
+		StoreMTLSCertificates(caCert, cert, key []byte) (string, string, string, error)
+		GetMTLSCertificates() (string, string, string, error)
 		GetDefaultChiselPrivateKeyPath() string
 		StoreChiselPrivateKey(privateKey []byte) error
 	}
