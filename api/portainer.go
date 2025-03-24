4320ecf756bbe4f814880a9a79a691f1e84d1b48
andres-portainer <91705312+andres-portainer@users.noreply.github.com>
2025-03-24 19:33:05 -0300

    feat(snapshots): avoid parsing raw snapshots when possible BE-11724 (#560)

--- api/portainer.go
+++ api/portainer.go
@@ -1624,7 +1624,7 @@ type (
 		Start()
 		SetSnapshotInterval(snapshotInterval string) error
 		SnapshotEndpoint(endpoint *Endpoint) error
-		FillSnapshotData(endpoint *Endpoint) error
+		FillSnapshotData(endpoint *Endpoint, includeRaw bool) error
 	}
 
 	// SwarmStackManager represents a service to manage Swarm stacks
