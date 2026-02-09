bachato/agent

fix(linter): add linter rules to reduce the chance for invalid FIPS settings BE-11979 (#975)

http/server.go
@@ -100,7 +100,7 @@ func (server *APIServer) Start(edgeMode bool) error {
 		return httpServer.ListenAndServe()
 	}
 
-	httpServer.TLSConfig = crypto.CreateTLSConfiguration()
+	httpServer.TLSConfig = crypto.CreateTLSConfiguration(false)
 
 	go server.securityShutdown(httpServer)
 

dfldylan/portainer

api/http/server.go
@@ -372,7 +372,7 @@ func (server *Server) Start() error {
 		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)), // Disable HTTP/2
 	}
 
-	httpsServer.TLSConfig = crypto.CreateTLSConfiguration()
+	httpsServer.TLSConfig = crypto.CreateTLSConfiguration(false)
 	httpsServer.TLSConfig.GetCertificate = func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
 		return server.SSLService.GetRawCertificate(), nil
 	}