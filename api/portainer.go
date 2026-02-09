c41f78ad7aeda67886d44561a06f30ddf6d1da04
jerry-yuan <i@jerryzone.cn>
2026-02-09 15:47:42 +0000

    feat(containers): add securityOpt

--- api/portainer.go
+++ api/portainer.go
@@ -2474,6 +2474,7 @@ func DefaultEndpointSecuritySettings() EndpointSecuritySettings {
 		AllowHostNamespaceForRegularUsers:         false,
 		AllowPrivilegedModeForRegularUsers:        false,
 		AllowSysctlSettingForRegularUsers:         false,
+		AllowSecurityOptForRegularUsers:           false,
 		AllowVolumeBrowserForRegularUsers:         false,
 		EnableHostManagementFeatures:              false,
 
